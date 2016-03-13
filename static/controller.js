7ff083efcf6bd419d7d75b876f3f020610b1bcf3
Kuei-chun Chen <simagix@gmail.com>
2016-03-13 13:06:50 +0000

    Add Angular UI

---
+++ static/controller.js
@@ -0,0 +1,35 @@
+/**
+ * controller.js
+ * Author: Ken Chen
+ */
+var app = angular.module('stream', ['ui.bootstrap']);
+
+app.controller('StreamController', function($scope, $http, $log) {
+    angular.element(document).ready(function() {
+        $log.info('ready!');
+        $scope.getTitles();
+    });
+
+    $scope.title = 'Select ...';
+    $scope.titles = [];
+
+    $scope.getTitles = function() {
+        var url = '/streaming/titles';
+        $http.get(url).success(function(data) {
+            $scope.titles = data;
+        });
+    }
+
+    $scope.titleSelected = function(doc) {
+        var url = '/streaming/udp/remote/1234/' + doc.index;
+        $scope.title = doc.title;
+        $http.post(url).success(function(data) {
+            $log.info('playing ...');
+            var video = document.getElementById('video');
+            video.playlist.clear();
+            video.playlist.add('udp://@:1234');
+            video.playlist.play(); 
+        });
+    }
+});
+
